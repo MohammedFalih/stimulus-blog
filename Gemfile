1a2efd9594026a9558cb899a3f52b4c55f372638
MohammedFalih <yunusfalih123@gmail.com>
2024-07-22 18:34:11 +0530

    final-commit

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "https://rubygems.org"
 
+gem "htmlbeautifier"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.0.beta3"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -2,6 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="visibility"
 export default class extends Controller {
+  static values = {
+    visible: Boolean
+    // visible: { type: Boolean, default: false}
+  }
   connect() {
+    this.updateVisibility()
+  }
+
+  visibleValueChanged() {
+    this.updateVisibility()
+  }
+
+  updateVisibility() {
+    if (this.visibleValue)
+      this.element.style.display = "block";
+    else {
+      this.element.style.display = "none";
+    }
   }
 }
